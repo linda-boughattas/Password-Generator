9a0863dff3cb2a4edf55bfc4af33bf3e1b02fa7d
Linda Boughattas <linda.boughattas@enicar.ucar.tn>
2024-06-25 19:50:36 +0200

    gave the user the option of adding/removing symbols and/or numbers

--- index.js
+++ index.js
@@ -1,21 +1,85 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+let characters =[ ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
+["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"],
+["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]]
 let firstPasswordEl=document.getElementById("firstPassword")
 let secondPasswordEl=document.getElementById("secondPassword")
 let random=""
+let Symbols=false
+let Numbers=false
+
+// letting user choose between adding symbols and characters or not
+document.addEventListener('DOMContentLoaded', function() {
+    let symbolsSwitch = document.getElementById('toggle-symbols');
+    let numbersSwitch = document.getElementById('toggle-numbers');
+
+    if (symbolsSwitch) {
+        symbolsSwitch.addEventListener('change', function() {
+            if (this.checked) {
+                Symbols=true   
+            } else { 
+                Symbols=false
+            }
+        });
+    }
+
+    if (numbersSwitch) {
+        numbersSwitch.addEventListener('change', function() {
+            if (this.checked) {
+                Numbers=true
+            } else {
+                Numbers=false
+            }
+        });
+    }
+});
 
 function Generate(){
     let myNumberEl = document.getElementById("myNumber").value;
-    Restart()
-    for(let i=0; i<myNumberEl; i++){
-        random=characters[Math.floor(Math.random() * characters.length)]
-        firstPasswordEl.textContent+=random
-        random=characters[Math.floor(Math.random() * characters.length)]
-        secondPasswordEl.textContent+=random
+    update()
+    if (!Symbols && !Numbers) {
+        for(let i=0; i<myNumberEl; i++){
+            random=characters[0][Math.floor(Math.random() * characters[0].length)]
+            firstPasswordEl.textContent+=random
+            random=characters[0][Math.floor(Math.random() * characters[0].length)]
+            secondPasswordEl.textContent+=random
+        }
+    } else if (Symbols && !Numbers){
+        for(let i=0; i<myNumberEl; i++){
+            let j= Math.floor(Math.random() * 2)
+            random=characters[j][ Math.floor(Math.random() * characters[j].length)]
+            firstPasswordEl.textContent+=random
+
+            j= Math.floor(Math.random() * 2)
+            random=characters[j][ Math.floor(Math.random() * characters[j].length)]
+            secondPasswordEl.textContent+=random
+        }
+    } else if (!Symbols && Numbers){
+        for(let i=0; i<myNumberEl; i++){
+            let j= Math.floor(Math.random() * 2) 
+            if (j=== 1){j+=1}
+            random=characters[j][ Math.floor(Math.random() * characters[j].length)]
+            firstPasswordEl.textContent+=random
+
+            j= Math.floor(Math.random() * 2)
+            if (j=== 1){j+=1}
+            random=characters[j][ Math.floor(Math.random() * characters[j].length)]
+            secondPasswordEl.textContent+=random
+        }
+    } else {
+        for(let i=0; i<myNumberEl; i++){
+            let j= Math.floor(Math.random() * 3)
+            random=characters[j][ Math.floor(Math.random() * characters[j].length)]
+            firstPasswordEl.textContent+=random
+
+            j= Math.floor(Math.random() * 3)
+            random=characters[j][ Math.floor(Math.random() * characters[j].length)]
+            secondPasswordEl.textContent+=random
+        } 
     }
+
 }
 
-function Restart(){
+function update(){
     firstPasswordEl.textContent =""
     secondPasswordEl.textContent =""
 }
